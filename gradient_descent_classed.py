4594308c46b83959a5f48eba7d4538a34254a00b
NibnibTM <davidkonovalovs@gmail.com>
2023-05-21 18:28:55 +0100

    classes finished

--- gradient_descent_classed.py
+++ gradient_descent_classed.py
@@ -12,11 +12,8 @@ class input_layers:
         #initialise output as zero array so instance of input_layers can be used as instance of layer class
         self.output = np.zeros(self.length)
 
-        self.labels_length = len(labels_list)
+        self.labels_length = int(max(labels_list) + 1)
         self.label = labels_list
-
-
-
 
 class layer:
     #i will need to somehow fill in the last layer's length into this
@@ -28,12 +25,13 @@ class layer:
 
         #Glorot uniform initialization = sqrt(6/(nodes in and nodes out)), input_size is last_layer.length
         self.initial_range = math.sqrt(6/(self.length + last_layer_length))
+        #self.initial_range = 0.2 #for some reason this breaks the algorithm lol, look into this
 
         self.weights = np.random.uniform(low=-self.initial_range, high=self.initial_range, size=(last_layer_length, self.length))
-        self.biases = np.random.uniform(low=-self.initial_range, high=self.initial_range, size=(length))
+        self.biases = np.random.uniform(low=-self.initial_range, high=self.initial_range, size=(length)) 
 
-        self.weights_grad_total = np.zeros((last_layer_length, self.length))
-        self.bias_grad_total = np.zeros((self.length))
+        self.weights_grad = np.zeros((last_layer_length, self.length))
+        self.bias_grad = np.zeros((self.length))
 
         self.delta = np.zeros(self.length)
 
@@ -46,12 +44,12 @@ class layer:
 
 #these activ funcs are applied to whole layer to utilise numpy
     def leaky_relu(self, weighted_sum):
-        self.output = np.where(weighted_sum > 0, weighted_sum, weighted_sum * 0.01)
+        self.output = np.where(weighted_sum > 0, weighted_sum, weighted_sum * 0.1)
         return self.output
     
     def leaky_relu_deriv(self, weighted_sum):
         self.output = np.where(weighted_sum < 0, weighted_sum, 1)
-        self.output = np.where(weighted_sum > 0, weighted_sum, 0.01)
+        self.output = np.where(weighted_sum > 0, weighted_sum, 0.1)
         return self.output
 
     def softmax(self, weighted_sum):
@@ -64,22 +62,16 @@ class layer:
 
     #takes weighted sum and performs activation function to get layer output
     def activate(self, weighted_sum):
-        self.output = self.leaky_relu(weighted_sum) #for now i cant get activ_func working so ill only use leaky_relu instead of all activs
+        self.output = self.activ_func(self, weighted_sum) 
         return self.output
     
     def deriv(self, weighted_sum):
-        self.output = self.leaky_relu_deriv(weighted_sum)
+        self.output = self.activ_func_deriv(weighted_sum)
         return self.output
     
     def get_deriv(self, activ_func):
-        deriv_funcs = {
-            'leaky_relu': self.leaky_relu_deriv,
-            #only relu needed for now
-        }
-        return deriv_funcs.get(activ_func)
-
-
-
+        if activ_func == layer.leaky_relu:
+            return self.leaky_relu_deriv
 
 
 L = []
@@ -98,33 +90,54 @@ def sgd(epochs, batch_size, learn_rate):
     num_layers = len(L)
 
     for h in range(epochs):
+        expected_prob_total = 0
+        cost_total = 0 
 
+        rand_list = np.random.choice(range(0, 60000), size=batch_size, replace=False)
         for i in range(batch_size):
+            image_no = int(rand_list[i])
 
-            L[0].output = L[0].image_array_list[i,:]
+            L[0].output = L[0].image_array_list[image_no,:]
 
-
+        #forward prop
             for m in range(1, num_layers):
                 L[m].weighted = L[m].weights_and_biases(L[m-1].output) 
                 L[m].output = L[m].activate(L[m].weighted)
 
             #zero hot array
             actual_probabality = np.zeros((L[0].labels_length))
-            actual_probabality[int(L[0].label)] = 1
+            actual_probabality[int(L[0].label[image_no])] = 1
 
-            #output is the first layer done, it has different calcs
-            L[num_layers].delta = np.zeros(L[0].labels_length)
-            L[num_layers].delta = L[num_layers].output - actual_probabality
+            expected_prob = L[num_layers-1].output[int(L[0].label[image_no])]
+            cost_total += -math.log(1e-9 + expected_prob)
+            expected_prob_total += expected_prob
 
-            L[m].biases_grad = L[num_layers].delta
-            L[m].weights_grad = np.outer(L[num_layers-1].output, L[num_layers].delta)
+        #backprop
+            #output L[num_layers-1] is the first layer done, it has different calcs
+            L[num_layers-1].delta = np.zeros(L[0].labels_length)
+            L[num_layers-1].delta = L[num_layers-1].output - actual_probabality
 
-            for m in range(num_layers - 1 - m, 1): #idk about syntax make sure does not drop below 1 which would lead to input layer which has no weights and biases
+            L[m].biases_grad = L[num_layers-1].delta
+            L[m].weights_grad = np.outer(L[num_layers-2].output, L[num_layers-1].delta)
 
-                L[m].delta = L[m].deriv(L[m].weighted_sum) * np.dot(L[m+1].delta, L[m+1].weights.T)
+            m = num_layers - 2
+            while m > 0 : #idk about syntax make sure does not drop below 1 which would lead to input layer which has no weights and biases
+
+                L[m].delta = L[m].deriv(L[m].weighted) * np.dot(L[m+1].delta, L[m+1].weights.T)
 
                 L[m].biases_grad = L[m].delta 
                 L[m].weights_grad = np.outer(L[m-1].output, L[m].delta)
+
+                m -= 1
+
+            for m in range(1, num_layers):
+                L[m].weights -= learn_rate * L[m].weights_grad
+                L[m].biases -= learn_rate * L[m].bias_grad
+            
+        learn_rate = learn_rate/1.2
+
+        print("Epoch:", h + 1, "Accuracy:", expected_prob_total/batch_size, "Cost:", cost_total/batch_size)
+            
 
 
 
--- main_classed.py
+++ main_classed.py
@@ -1,6 +1,28 @@
 import gradient_descent_classed
 import numpy as np
 
+x_train = np.load('x_train.npy')
+y_train = np.load('y_train.npy')
+x_test = np.load('x_test.npy')
+y_test = np.load('y_test.npy')
+
+# Print the shape of the data
+""" print("X_train shape:", x_train.shape)
+print("y_train shape:", y_train.shape) """
+
+
+def train():
+    gradient_descent_classed.input(x_train, y_train)
+
+    gradient_descent_classed.add(gradient_descent_classed.layer.leaky_relu, 256)
+    gradient_descent_classed.add(gradient_descent_classed.layer.leaky_relu, 128)
+
+    gradient_descent_classed.add(gradient_descent_classed.layer.softmax, 10)
+
+    gradient_descent_classed.sgd(10, 1785, 0.005)
+
+#def test():
+
 #quick exit code
 import signal
 def exit_gracefully(signal, frame):
@@ -9,30 +31,6 @@ def exit_gracefully(signal, frame):
 
 # Register the exit function to be called on SIGINT (Ctrl+C)
 signal.signal(signal.SIGINT, exit_gracefully) 
-
-x_train = np.load('x_train.npy')
-y_train = np.load('y_train.npy')
-x_test = np.load('x_test.npy')
-y_test = np.load('y_test.npy')
-
-# Print the shape of the data
-print("X_train shape:", x_train.shape)
-print("y_train shape:", y_train.shape)
-
-
-def train():
-    gradient_descent_classed.input(x_train, y_train)
-
-    gradient_descent_classed.add(gradient_descent_classed.layer.leaky_relu, 256)
-    gradient_descent_classed.add(gradient_descent_classed.layer.leaky_relu, 128)
-    gradient_descent_classed.add(gradient_descent_classed.layer.leaky_relu, 128)
-
-    gradient_descent_classed.add(gradient_descent_classed.layer.softmax, 10)
-
-    gradient_descent_classed.sgd(10, 1024, 0.005)
-
-#def test():
-
 
 train()
 #test()
